c736b9b30d2140547b4a83f99401b80189e5b891
Miguel Fernandez <miguel.andresff@gmail.com>
2016-12-20 17:46:40 -0400

    Empezando trab. clase tablero

---
+++ casilla.py
@@ -0,0 +1,9 @@
+class Casilla:
+
+    def __init__(self):
+        self.contenido = None
+        self.cor_tablero = None
+        self.cor_pixeles = None
+
+    def __str__(self):
+        pass
--- tablero.py
+++ tablero.py
@@ -1,8 +1,17 @@
 class Tablero:
 
     def __init__(self):
+
+        self.matriz = 8 * [ 8 * [None]]
+
         pass
 
+a = Tablero()
 
+for a in (a.matriz):
+    for b in a:
+        print(b)
+
+    print(a)
 
 
