3e0d1a2b38622ac5ec3d27b10870b43a30c3e78d
Miguel Fernandez <miguel.andresff@gmail.com>
2017-01-06 13:50:38 -0400

    Crea variables globales para nom. fichas
    Para tener la posibilidad mas adelante de cambiar
    el nombre de los archivos en un solo sitio.

--- tablero.py
+++ tablero.py
@@ -6,9 +6,16 @@ class Tablero:
 
     def __init__(self):
 
-        self._matriz = [[None for f in range(8)] for c in range(8)]
+        self._matriz = [[None for f in range(8)] for c in range(8)]   # Matriz del tablero
         self.filas_ini_sup = [0, 1, 2]                                # Filas iniciales en la parte superior del tablero.
         self.filas_ini_inf = [5, 6, 7]                                # Filas iniciales en la parte inferior del tablero.
+        self.ficha_1 = 'f_blanca_.png'                                # Variable global para hacer el juego independiente de la ficha
+        self.ficha_1_rey = 'f_blanca_rey_.png'
+        self.ficha_2 = 'f_marron_.png'                                # Variable global para hacer el juego independiente de la ficha
+        self.ficha_2_rey = 'f_marron_rey_.png'
+
+        self.cont_f1 = 0                                              # Para contar las fichas comidas
+        self.cont_f2 = 0                                              # Para contar las fichas comidas
 
 
     def inicializar_tablero(self):
@@ -34,11 +41,11 @@ class Tablero:
 
                 # Por el momento solo se estara utilizando una ficha para pruebas.
                 if f == 2 and c == 1:                                             #(x_izq, x_der, y_arriba, y_abajo )
-                    self._matriz[f][c] = Casilla (Ficha('f_blanca_.png', f, cont_pixels_ficha_x, cont_pixels_ficha_y), (f, c), (cont_pixels_x - pixels_cuadro, cont_pixels_x, cont_pixels_y - pixels_cuadro, cont_pixels_y), color)
+                    self._matriz[f][c] = Casilla (Ficha(self.ficha_1, f, cont_pixels_ficha_x, cont_pixels_ficha_y), (f, c), (cont_pixels_x - pixels_cuadro, cont_pixels_x, cont_pixels_y - pixels_cuadro, cont_pixels_y), color)
 
 
                 elif f == 5 and c == 2:                                             #(x_izq, x_der, y_arriba, y_abajo )
-                    self._matriz[f][c] = Casilla (Ficha('f_marron_.png', f, cont_pixels_ficha_x, cont_pixels_ficha_y), (f, c), (cont_pixels_x - pixels_cuadro, cont_pixels_x, cont_pixels_y - pixels_cuadro, cont_pixels_y), color)
+                    self._matriz[f][c] = Casilla (Ficha(self.ficha_2, f, cont_pixels_ficha_x, cont_pixels_ficha_y), (f, c), (cont_pixels_x - pixels_cuadro, cont_pixels_x, cont_pixels_y - pixels_cuadro, cont_pixels_y), color)
 
                 else:
                     self._matriz[f][c] = Casilla(None, (f, c), (cont_pixels_x - pixels_cuadro, cont_pixels_x, cont_pixels_y - pixels_cuadro, cont_pixels_y), color)
@@ -81,7 +88,7 @@ class Tablero:
 
         casilla = self._matriz[f][c]
 
-        if casilla_vieja.ficha.nom_archivo == 'f_blanca_.png' or casilla_vieja.ficha.nom_archivo == 'f_marron_.png':      # Logica mov. fichas simpoles
+        if casilla_vieja.ficha.nom_archivo == self.ficha_1 or casilla_vieja.ficha.nom_archivo == self.ficha_2:      # Logica mov. fichas simpoles
 
             if casilla_vieja.ficha.fila_inicial in self.filas_ini_sup:                                                                                              # Para fichas inicializadas en la filas superiores, solo pueden descender diagonalmente.
 
@@ -94,7 +101,7 @@ class Tablero:
                     return True
 
 
-        if casilla_vieja.ficha.nom_archivo == 'f_blanca_rey_.png' or casilla_vieja.ficha.nom_archivo == 'f_marron_rey_.png':
+        if casilla_vieja.ficha.nom_archivo == self.ficha_1_rey or casilla_vieja.ficha.nom_archivo == self.ficha_2_rey:
 
             if casilla.ficha == None and casilla.color == True and ((f - 1) == f_vieja or (f + 1) == f_vieja) and (c + 1 == c_vieja or c - 1 == c_vieja):  # El mov. es valido si la ficha a sido movida a una casilla solida, ha decendido una fila.
                                                                                                                                                            # ,subido una fila, aumentado una columna o aumentado una fila (ficha rey).
@@ -110,7 +117,7 @@ class Tablero:
 
         casilla = self._matriz[f][c]
 
-        if casilla_vieja.ficha.nom_archivo == 'f_blanca_.png' or casilla_vieja.ficha.nom_archivo == 'f_marron_.png':                       # Logica salto men
+        if casilla_vieja.ficha.nom_archivo == self.ficha_1 or casilla_vieja.ficha.nom_archivo == self.ficha_2:                       # Logica salto men
 
             if casilla_vieja.ficha.fila_inicial in self.filas_ini_sup:                                                                     # Para fichas inicializadas en la filas superiores, solo pueden descender diagonalmente.
                                                                                                                                            # El salto es valido si la ficha a sido movida a una casilla solida, ha asendido una fila,
@@ -151,7 +158,7 @@ class Tablero:
 
                     #return True
 
-        if casilla_vieja.ficha.nom_archivo == 'f_blanca_rey_.png' or casilla_vieja.ficha.nom_archivo == 'f_marron_rey_.png':                               # Para determinar saltos validos, reyes
+        if casilla_vieja.ficha.nom_archivo == self.ficha_1_rey or casilla_vieja.ficha.nom_archivo == self.ficha_2_rey:                               # Para determinar saltos validos, reyes
 
             if casilla.ficha == None and casilla.color == True and ((f - 2) == f_vieja or (f + 2) == f_vieja) and (c + 2 == c_vieja or c - 2 == c_vieja):  # El mov. es valido si la ficha a sido movida a una casilla solida, ha decendido una fila.
                                                                                                                                                             # ,subido una fila, aumentado una columna o aumentado una fila (ficha rey).
@@ -196,13 +203,13 @@ class Tablero:
 
         if f == 0 or f == 7:                            # En caso de que se llege a kings row
 
-            if ficha.nom_archivo == 'f_marron_.png':     # Coronando la ficha marron
-                ficha = Ficha('f_marron_rey_.png', f, x, y)
+            if ficha.nom_archivo == self.ficha_2:     # Coronando la ficha marron
+                ficha = Ficha(self.ficha_2_rey, f, x, y)
                 ficha.rect.x = x
                 ficha.rect.y = y
 
-            if ficha.nom_archivo == 'f_blanca_.png':     # Coronando la ficha blanca
-                ficha = Ficha('f_blanca_rey_.png', f, x, y)
+            if ficha.nom_archivo == self.ficha_1:     # Coronando la ficha blanca
+                ficha = Ficha(self.ficha_1_rey, f, x, y)
                 ficha.rect.x = x
                 ficha.rect.y = y
 
@@ -216,11 +223,19 @@ class Tablero:
         f, c = self.det_casilla(x, y)
         return self._matriz[f][c]
 
+
+    def remover_ficha(self, casilla):
+        """Para remover una ficha del tablero """
+        
+        #if casilla.ficha.tipo_color
+
+        pass
+
 #a = Tablero()
 #
 # ma = a.inicializar_tablero()
 #
-# ma[0][0] = Casilla (Ficha('f_marron_.png'), (0, 0), (100, 0))
+# ma[0][0] = Casilla (Ficha(self.ficha_2), (0, 0), (100, 0))
 
 
 
